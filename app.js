404eacafb7ef3f87aabd9fa43dbad0b717b3965d
Ranim <i502848@fhict.nl>
2023-05-26 22:53:46 +0200

    d

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ client.connect().then(() => {
 
     app.use((request, response, next) => {
         response.header("Access-Control-Allow-Origin", "*");
+        response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
-    })
+    });
+
 
     app.use(express.static(path.join(__dirname, 'public')));
 
