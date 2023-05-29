37cc96edf406c0c859d6493f3d9e76aa2307490c
Ranim <i502848@fhict.nl>
2023-05-29 12:33:32 +0200

    f

--- app.js
+++ app.js
@@ -1,85 +1,138 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const bodyParser = require('body-parser');
-
+const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
-const uri = "mongodb+srv://ranimm01:I729kBQsuURfmgBw@cluster0.fsmsdt5.mongodb.net/?retryWrites=true&w=majority";
 
-//  ================================================ mongodb connect ================================================= //
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+const bodyParser = require('body-parser');
+const port = 3000;
+
+//serve files from public directory
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// define routes
+app.get('/', (req, res) => {
+    res.sendFile(indexPath);
+});
+
+// =========================================the post request===============================================//
+// add a new route to handle POST requests to /tweet
+app.post('/tweet', async (req, res) => {
+    try {
+        const tweetText = req.body.tweetText;
+        const timestamp = new Date().toLocaleTimeString();
+        const newTweet = {
+            author: 'Ranim',
+            username: 'Reynim',
+            profile_image:
+                'https://t3.ftcdn.net/jpg/05/58/82/34/240_F_558823483_uP4but5eEUX1ub7sY9As43YJ4er67J4E.jpg',
+            timestamp: timestamp,
+            content: tweetText,
+        };
+
+        const filePath = path.join(__dirname, 'public', 'js', 'mock-data.json');
+        const jsonData = await fs.promises.readFile(filePath, 'utf8');
+        const data = JSON.parse(jsonData);
+
+        data.push(newTweet);
+
+        await fs.promises.writeFile(filePath, JSON.stringify(data));
+
+        res.json(newTweet);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal server error');
     }
 });
 
-//  ===================================== receiving the data from db and defining the routes ======================== // 
-client.connect().then(() => {
-    const database = client.db("test")
-    const collection = database.collection("fuck")
-
-    app.use(bodyParser.json());
-
-    app.use((request, response, next) => {
-        response.header("Access-Control-Allow-Origin", "*");
-        next();
-    })
-
-    app.use(express.static(path.join(__dirname, 'public')));
-
-    app.get('/', (request, response) => {
-        response.sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
 
 
-    app.get('/tweets/', (request, response) => {
-        console.log("request comes in" + request)
-        response.setHeader('Content-Type', 'application/json');
-        collection.find().toArray()
-            .then((documents) => {
-                response.setHeader('Content-Type', 'application/json');
-                response.status(200).json(documents);
-            })
-            .catch((error) => {
-                console.error(error);
-                response.status(500).json({ error: 'Internal server error' });
-            });
-    });
+// const express = require('express');
+// const fs = require('fs');
+// const path = require('path');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const bodyParser = require('body-parser');
 
 
-    // =========================================the post request===============================================//
-    app.post('/tweet/', (req, res) => {
-        try {
-            const tweetText = req.body.tweetText;
-            const timestamp = new Date().toLocaleTimeString();
-            const newTweet = {
-                author: 'Ranim',
-                username: 'Reynim',
-                profile_image:
-                    'https://t3.ftcdn.net/jpg/05/58/82/34/240_F_558823483_uP4but5eEUX1ub7sY9As43YJ4er67J4E.jpg',
-                timestamp: timestamp,
-                content: tweetText,
-            };
+// const app = express();
+// const port = process.env.PORT || 3000;
+// const uri = "mongodb+srv://ranimm01:I729kBQsuURfmgBw@cluster0.fsmsdt5.mongodb.net/?retryWrites=true&w=majority";
 
-            collection
-                .insertOne(newTweet)
-                .then(() => {
-                    res.json(newTweet);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    res.status(500).send('Internal server error');
-                });
-        } catch (err) {
-            console.error(err);
-            res.status(500).send('Internal server error');
-        }
-    });
+// //  ================================================ mongodb connect ================================================= //
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
+
+// //  ===================================== receiving the data from db and defining the routes ======================== // 
+// client.connect().then(() => {
+//     const database = client.db("test")
+//     const collection = database.collection("fuck")
+
+//     app.use(bodyParser.json());
+
+//     app.use((request, response, next) => {
+//         response.header("Access-Control-Allow-Origin", "*");
+//         next();
+//     })
+
+//     app.use(express.static(path.join(__dirname, 'public')));
+
+//     app.get('/', (request, response) => {
+//         response.sendFile(path.join(__dirname, 'public', 'index.html'));
+//     });
+
+
+//     app.get('/tweets/', (request, response) => {
+//         console.log("request comes in" + request)
+//         response.setHeader('Content-Type', 'application/json');
+//         collection.find().toArray()
+//             .then((documents) => {
+//                 response.setHeader('Content-Type', 'application/json');
+//                 response.status(200).json(documents);
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//                 response.status(500).json({ error: 'Internal server error' });
+//             });
+//     });
+
+
+//     // =========================================the post request===============================================//
+//     app.post('/tweet/', (req, res) => {
+//         try {
+//             const tweetText = req.body.tweetText;
+//             const timestamp = new Date().toLocaleTimeString();
+//             const newTweet = {
+//                 author: 'Ranim',
+//                 username: 'Reynim',
+//                 profile_image:
+//                     'https://t3.ftcdn.net/jpg/05/58/82/34/240_F_558823483_uP4but5eEUX1ub7sY9As43YJ4er67J4E.jpg',
+//                 timestamp: timestamp,
+//                 content: tweetText,
+//             };
+
+//             collection
+//                 .insertOne(newTweet)
+//                 .then(() => {
+//                     res.json(newTweet);
+//                 })
+//                 .catch((err) => {
+//                     console.error(err);
+//                     res.status(500).send('Internal server error');
+//                 });
+//         } catch (err) {
+//             console.error(err);
+//             res.status(500).send('Internal server error');
+//         }
+//     });
 
 
     // ======================================start server======================================== //
@@ -87,4 +140,4 @@ client.connect().then(() => {
         console.log(`Example app listening on port ${port}`);
     });
 
-})
+// })
