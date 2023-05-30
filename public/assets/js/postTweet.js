8d98324083610b8be493f52d872bc32898c88f53
Ranim <i502848@fhict.nl>
2023-05-30 22:36:04 +0200

    d

--- public/assets/js/postTweet.js
+++ public/assets/js/postTweet.js
@@ -6,7 +6,7 @@ $('.form').submit(function (event) {
 
     //ajax for the post method
     $.ajax({
-        url: '/tweets/',
+        url: '/netlify/functions/tweet',
         method: 'POST',
         contentType: 'application/json',
         data: JSON.stringify({ tweetText: tweetText }),
