77f3b4b3725bf35f61e5a606bbcdff167c6fe056
Ranim <i502848@fhict.nl>
2023-05-30 22:24:54 +0200

    d

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const tweetsRouter = require('/.netlify/functions/tweet');
+const tweetsRouter = require('./netlify/functions/tweet');
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
