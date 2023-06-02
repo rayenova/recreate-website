01e1993f099e9d4fcc9574349931399090d5d488
Ranim <i502848@fhict.nl>
2023-06-02 11:47:06 +0200

    d

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
-const tweetsRouter = require('./netlify/functions/tweet');
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,8 +11,36 @@ app.use(bodyParser.json());
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/tweets', tweetsRouter);
+// Define the tweet handler
+const tweetHandler = async (req, res) => {
+    try {
+        const { tweetText } = req.body;
+        const timestamp = new Date().toLocaleTimeString();
+        const newTweet = {
+            author: 'Ranim',
+            username: 'Reynim',
+            profile_image: 'https://t3.ftcdn.net/jpg/05/58/82/34/240_F_558823483_uP4but5eEUX1ub7sY9As43YJ4er67J4E.jpg',
+            timestamp: timestamp,
+            content: tweetText,
+        };
+
+        const filePath = path.resolve(__dirname, 'public/assets/js/mockdata.json');
+        const jsonData = await fs.promises.readFile(filePath, 'utf8');
+        const data = JSON.parse(jsonData);
+
+        data.push(newTweet);
+
+        await fs.promises.writeFile(filePath, JSON.stringify(data));
+
+        return res.status(200).json(newTweet);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'An internal server error occurred' });
+    }
+};
+
+// Endpoint to handle the tweet submission
+app.post('/tweets', tweetHandler);
 
 // Default route
 app.get('/', (req, res) => {
